04a43347080de1a4cd931e35f3d998d1e92f2cd6
TAEWOO KIM <taewookimmr@gmail.com>
2019-12-30 23:25:02 +0900

    python nibbler almost completed

--- python/nibbler.py
+++ python/nibbler.py
@@ -4,11 +4,19 @@ import threading
 import queue 
 import random 
 
+KEEP = -1
 RIGHT = 0
 LEFT = 1
 UP = 2
 DOWN = 3
 
+dirList = [RIGHT, LEFT, UP, DOWN]
+
+dir = {}
+dir[RIGHT] = [0,1]
+dir[LEFT]  = [0,-1]
+dir[UP]    = [-1,0]
+dir[DOWN]  = [1,0]
 
 class Nibbler(threading.Thread):
 
@@ -22,11 +30,11 @@ class Nibbler(threading.Thread):
         self.applePosition = -1 # 사과의 위치
         self.appleEatten = False # 사과 먹었는 지 유무
 
+
         self.root = None
         self.cvs = None
         self.grids = grids
-
-    
+ 
     def run(self):
         self.init_nibbler()
         while self.runFlag:
@@ -38,15 +46,10 @@ class Nibbler(threading.Thread):
         
             if self.is_apple_eatten():
                 self.add_tail()
-                self.applePosition = -1
-            
+
             self.determine_dir()
             time.sleep(1/self.speed)
             
-        
-
-
-
     def init_nibbler(self):
         self.headDir = 0
         self.body = [] 
@@ -58,18 +61,15 @@ class Nibbler(threading.Thread):
             c = self.body[i] % self.mapsize
             self.draw(r, c)
 
-
     def draw(self, row, col, color='black'):
         b= self.grids[row*self.mapsize+col]
         b['bg'] = color
 
-
     def add_tail(self):
-        prevTail = body[-1]
+        prevTail = self.body[-1]
         r = prevTail//self.mapsize
         c = prevTail %self.mapsize
-        dir = [[-1, 0], [0, -1], [1,0], [0,1]]
-        for dr, dc in dir:
+        for dr, dc in dir.values():
             nr = r + dr
             nc = c + dc 
             if nr >= 0 and nr < self.mapsize and nc >=0 and nc < self.mapsize:
@@ -81,62 +81,231 @@ class Nibbler(threading.Thread):
         else:
             print("fail to add tail")
 
- 
-
     def move(self):
-        r = self.body[0] // self.mapsize
-        c = self.body[0] % self.mapsize
-
-        nr,nc = r,c # 새로운 row, col
-
-        # boundary condition check
-        if self.headDir == RIGHT:
-            if c+1 < self.mapsize:
-                nc+=1
-            else :
-                return False # 죽는다
-        elif self.headDir == LEFT : 
-            if c-1 >= 0 :
-                nc -=1
-            else :
-                return False
-        elif self.headDir == UP :
-            if r-1 >= 0 :
-                nr -=1
-            else :
-                return False
-        elif self.headDir == DOWN:
-            if r+1 < self.mapsize:
-                nr +=1
-            else : 
-                return False
-        
-        np = nr * self.mapsize + nc # 일단 boundary condition은 통과 
-        
-        # self body touch check
-        if np not in self.body:
+        cango, nr, nc = self.is_able_to_go()
+        if cango:
+            np = nr*self.mapsize+nc
             # 현재 꼬리를 지우고, 새로운 머리에 붙이는 방식으로 이동한다.
             self.grids[self.body[-1]]['bg']='gray'
             self.body.pop()
             self.body.insert(0, np)
-            self.grids[self.body[0]]['bg'] ='red'
+            self.grids[self.body[0]]['bg'] ='blue'
             self.grids[self.body[1]]['bg'] ='black'
             return True 
         else:
+            for p in self.body:
+                self.grids[p]['bg']='orange'
             return False
 
-    
-    def is_able_to_go(self, direction):
-        pass
+    def is_able_to_go(self, newDir=KEEP):
+        r = self.body[0] // self.mapsize
+        c = self.body[0] % self.mapsize
+        nr,nc = r,c # 새로운 row, col
+        if newDir == KEEP:
+            # boundary condition check
+            if self.headDir == RIGHT:
+                if c+1 < self.mapsize:
+                    nc+=1
+                else :
+                    return False , -1 , -1
+            elif self.headDir == LEFT : 
+                if c-1 >= 0 :
+                    nc -=1
+                else :
+                    return False, -1 , -1
+            elif self.headDir == UP :
+                if r-1 >= 0 :
+                    nr -=1
+                else :
+                    return False, -1 , -1
+            elif self.headDir == DOWN:
+                if r+1 < self.mapsize:
+                    nr +=1
+                else : 
+                    return False, -1 , -1
+            
+            np = nr * self.mapsize + nc # 일단 boundary condition은 통과 
+
+            if np not in self.body:
+                return True, nr, nc
+            else:
+                return False, -1, -1
+
+        else :
+            dr,dc = dir[newDir]
+            nr, nc = r+dr, c+dc
+            if newDir == RIGHT and nc >= self.mapsize:
+                return False, -1,-1
+            elif newDir == LEFT and nc < 0:
+                return False, -1, -1
+            elif newDir == UP and nr < 0:
+                return False, -1, -1
+            elif newDir == DOWN and nr >= self.mapsize:
+                return False, -1, -1
+        
+            np = nr*self.mapsize+nc
+            if np not in self.body:
+                return True, nr, nc
+            else:
+                return False, -1, -1
 
     def determine_dir(self):
-        pass
+        hr = self.body[0] // self.mapsize
+        hc = self.body[0] % self.mapsize
+
+        ar = self.applePosition // self.mapsize
+        ac = self.applePosition % self.mapsize
+
+        dr = ar - hr
+        dc = ac - hc
+        
+        right,left,up,down = [False]*4
+        for nextdir in dirList:
+            if self.headDir == nextdir:
+                 right = self.is_able_to_go(RIGHT)
+                 left  = self.is_able_to_go(LEFT)
+                 up    = self.is_able_to_go(UP)
+                 down  = self.is_able_to_go(DOWN)
+        
+        r = dr if dr >=0 else -1
+        c = dc if dc >=0 else -1
+
+        if c == 0 :
+            if (r < 0 and up) :
+                self.headDir = UP 
+                return
+            
+            if r > 0 and down :
+                self.headDir = DOWN
+                return
+
+            if right:
+                self.headDir = RIGHT
+                return
+            
+            if left :
+                headDir = LEFT
+                return
+
+        if r == 0:
+
+            if c>0 and right :
+                self.headDir = RIGHT
+                return
+            
+            if c<0 and left :
+                self.headDir = LEFT
+                return
+            
+            if up:
+                self.headDir = UP
+                return
+
+            if down:
+                selfheadDir = DOWN
+                return
+            
+            
+        if c > 0:
+            if right :
+                self.headDir = RIGHT
+                return 
+        
+            if up and r < 0 : 
+                self.headDir = UP
+                return
+            
+            if down and r > 0:
+                self.headDir = DOWN
+                return
+            
+
+            if left:
+                self.headDir = LEFT
+                return
+        
+
+        if c < 0:
+
+            if left :
+                self.headDir = LEFT
+                return
+
+            if up and r < 0:
+                selfheadDir = UP
+                return
+        
+        
+            if down and r > 0:
+                self.headDir = DOWN
+                return
+        
+
+        
+            if right: 
+                headDir = RIGHT
+                return
+
+        if r > 0 :
+
+            if down:
+                headDir = DOWN
+                return
+
+
+            if right and c>0:
+                self.headDir = RIGHT
+                return
+            
+            if left and r<0:
+                self.headDir = LEFT
+                return
+            
+
+            if up:
+                self.headDir = UP
+                return
+
+        if r < 0 :
+
+            if up:
+                self.headDir = UP
+                return
+
+            if right and c>0:
+                self.headDir = RIGHT
+                return
+            
+            if left and c<0:
+                self.headDir = LEFT
+                return
+
+            if down:
+                self.headDir = DOWN
+                return
+
+        self.headDir = -1
+        self.runFlag = False
+        return 
+
+
+        
 
     def create_apple(self):
-        pass
+        while True:
+            p = random.randint(0, self.mapsize*self.mapsize-1)
+            if p not in self.body:
+                self.applePosition=p
+                self.grids[p]['bg'] = 'red'
+                self.appleEatten=False
+                break
 
     def is_apple_eatten(self):
-        pass
+        for p in self.body:
+            if p == self.applePosition:
+                self.applePosition = -1
+                return True
+        return False
 
 class Emulator():
     def __init__(self, size=20):
